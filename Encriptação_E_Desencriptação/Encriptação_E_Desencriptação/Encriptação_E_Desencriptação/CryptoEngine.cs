1a691d011a4206f5ea7a0a118b2bbe07d66d8e95
André Custódio <56931353+AndreZila01@users.noreply.github.com>
2021-02-16 09:06:45 +0000

    Remove .rar

---
+++ Encriptação_E_Desencriptação/Encriptação_E_Desencriptação/Encriptação_E_Desencriptação/CryptoEngine.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encriptação_E_Desencriptação
+{
+	public class CryptoEngineC
+	{
+		public static string Encrypt(string input, string key)
+		{
+			try
+			{
+				byte[] inputArray = UTF8Encoding.UTF8.GetBytes(input);
+				TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+				tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+				tripleDES.Mode = CipherMode.ECB;
+				tripleDES.Padding = PaddingMode.PKCS7;
+				ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+				byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+				tripleDES.Clear();
+				return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+			}
+			catch (Exception ex)
+			{
+				return "ERRO 404!! \n" + ex.Message;
+			}
+
+		}
+		public static string Decrypt(string input, string key)
+		{
+			try
+			{
+				byte[] inputArray = Convert.FromBase64String(input);
+				TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+				tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+				tripleDES.Mode = CipherMode.ECB;
+				tripleDES.Padding = PaddingMode.PKCS7;
+				ICryptoTransform cTransform = tripleDES.CreateDecryptor();
+				byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+				tripleDES.Clear();
+				return UTF8Encoding.UTF8.GetString(resultArray);
+			}
+			catch (Exception ex)
+			{
+				return "ERRO 404!! \n" + ex.Message;
+			}
+		}
+	}
+
+	public class CrytoEngineB
+	{
+		public static string Encrypt(string input)
+		{
+			try
+			{
+				int BlockSize = 128;
+				byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+				byte[] bytes = Encoding.Unicode.GetBytes(input);
+				//Encrypt
+				SymmetricAlgorithm crypt = Aes.Create();
+				HashAlgorithm hash = MD5.Create();
+				crypt.BlockSize = BlockSize;
+				//crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(textBoxPassword.Text));
+				crypt.IV = IV;
+
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(memoryStream, crypt.CreateEncryptor(), CryptoStreamMode.Write))
+					{
+						cryptoStream.Write(bytes, 0, bytes.Length);
+					}
+
+					return Convert.ToBase64String(memoryStream.ToArray());
+				}
+			}
+			catch (Exception ex)
+			{
+				return "ERRO 404!! \n" + ex.Message;
+			}
+
+		}
+		public static string Decrypt(string input)
+		{
+			try
+			{
+				byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+				byte[] bytes = Convert.FromBase64String(input);
+
+				SymmetricAlgorithm crypt = Aes.Create();
+				HashAlgorithm hash = MD5.Create();
+				//crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(textBoxPassword.Text));
+				crypt.IV = IV;
+
+				using (MemoryStream memoryStream = new MemoryStream(bytes))
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(memoryStream, crypt.CreateDecryptor(), CryptoStreamMode.Read))
+					{
+						byte[] decryptedBytes = new byte[bytes.Length];
+						cryptoStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+						return Encoding.Unicode.GetString(decryptedBytes);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return "ERRO 404!! \n" + ex.Message;
+			}
+		}
+	}
+}
---
+++ Encriptação_E_Desencriptação/Encriptação_E_Desencriptação/Encriptação_E_Desencriptação/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encriptação_E_Desencriptação
+{
+
+	public partial class Form1 : Form
+	{
+		#region variaveis globais
+		int widht = Screen.PrimaryScreen.Bounds.Width;
+		int height = Screen.PrimaryScreen.Bounds.Height;
+		Encriptação encriptação = Encriptação.Vazio;
+		#endregion
+
+		#region Form
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//this.BackColor = Color.LimeGreen;
+			//this.TransparencyKey = Color.LimeGreen;
+			this.Width = 860;
+			this.Height = 491;
+			encriptação = Encriptação.Vazio;
+		}
+
+		#endregion
+
+		enum Encriptação
+		{
+			Vazio,
+			EncriptaçãoA,
+			EncriptaçãoB,
+			EncriptaçãoC
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			encriptaredesencriptar();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			button1.Click += comboBox1_SelectedIndexChanged;
+			var d = button1.Text == "Desincriptar Texto" ? label1.Text = "Texto Encriptado" : label1.Text = "Texto";
+			d = button1.Text == "Desincriptar Texto" ? label2.Text = "Texto Desincriptado" : label2.Text = "Encriptação Texto";
+			d = button1.Text == "Desincriptar Texto" ? button1.Text = "Encriptação Texto" : button1.Text = "Desincriptar Texto";
+			var ds = button1.Text == "Desincriptar Texto" ? button1.Tag = "0" : button1.Tag = "1";
+
+			comboBox1.Text = string.Empty;
+
+			if (encriptação != Encriptação.Vazio)
+				encriptação = Encriptação.Vazio;
+
+			Debug.Print("" + encriptação);
+
+			textBox1.Text = string.Empty;
+			textBox2.Text = string.Empty;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var d = comboBox1.Text == "1- Encriptação A (fácil);" ? encriptação = Encriptação.EncriptaçãoA : (comboBox1.Text == "2- Encriptação B (médiano);" ? encriptação = Encriptação.EncriptaçãoB : (comboBox1.Text == "3- Encriptação C (dífcil e modificada);" ? encriptação = Encriptação.EncriptaçãoC : encriptação = Encriptação.Vazio));
+
+			Debug.Print("" + encriptação);
+
+			var dd = comboBox1.Text == "3- Encriptação C (dífcil e modificada);" ? textBox3.Visible = true : textBox3.Visible = false;
+			textBox3.Text = "sblw-3hn8-sqoy19";
+
+
+
+			encriptaredesencriptar();
+		}
+
+		private void encriptaredesencriptar()
+		{
+			textBox2.Text = string.Empty;
+
+			if (button1.Text == "Desincriptar Texto")
+				switch (encriptação)
+				{
+					case Encriptação.EncriptaçãoA:
+						{
+							try
+							{
+								if (textBox1.Text != string.Empty)
+								{
+									textBox2.Text = string.Empty;
+									for (int i = 0; i < textBox1.Text.Length; i++)
+									{
+										int txtUsuario = (int)textBox1.Text[i];
+										int txtCifrado = txtUsuario + 10;
+										textBox2.Text += Char.ConvertFromUtf32(txtCifrado);
+									}
+								}
+							}
+							catch (Exception ex)
+							{
+								textBox2.Text= "ERRO 404!! \n" + ex.Message;
+							}
+						}
+							break;
+					case Encriptação.EncriptaçãoB:
+						var d = textBox1.Text != string.Empty ? textBox2.Text = CrytoEngineB.Encrypt(textBox1.Text) : null;
+						break;
+					case Encriptação.EncriptaçãoC:
+						d = textBox1.Text != string.Empty ? textBox2.Text = CryptoEngineC.Encrypt(textBox1.Text, "sblw-3hn8-sqoy19") : null;
+						break;
+					default:
+						break;
+				}
+			else
+			if (button1.Text == "Encriptação Texto")
+				switch (encriptação)
+				{
+					case Encriptação.EncriptaçãoA:
+						{
+							try
+							{
+								if (textBox1.Text != string.Empty)
+								{
+									textBox2.Text = string.Empty;
+									for (int i = 0; i < textBox1.Text.Length; i++)
+									{
+										int txtCifrado = (int)textBox1.Text[i];
+										int txtOriginal = txtCifrado - 10;
+										textBox2.Text += Char.ConvertFromUtf32(txtOriginal);
+									}
+								}
+
+							}
+							catch (Exception ex)
+							{
+								textBox2.Text = "ERRO 404!! \n" + ex.Message;
+							}
+						}
+						break;
+					case Encriptação.EncriptaçãoB:
+						var d = textBox1.Text != string.Empty ? textBox2.Text = CrytoEngineB.Decrypt(textBox1.Text) : null;
+						break;
+					case Encriptação.EncriptaçãoC:
+						d = textBox1.Text != string.Empty ? textBox2.Text = CryptoEngineC.Decrypt(textBox1.Text, "sblw-3hn8-sqoy19") : null;
+						break;
+					default:
+						break;
+				}
+		}
+
+		
+	}
+}
