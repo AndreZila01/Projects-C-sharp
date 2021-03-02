909b2aa753c0cace7a513c006feaa4260102451e
André Custódio <56931353+AndreZila01@users.noreply.github.com>
2021-03-02 11:04:04 +0000

    Update in pokemon, other files not saved
    In github, lose my files on program pokemon

---
+++ Pokedex_Pokemon/Pokedex Pokémon/Pokedex Pokémon/FormAuxVertical.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Pokedex_Pokémon
+{
+	partial class FormAuxVertical
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuxVertical));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.lblInfo = new System.Windows.Forms.Label();
+			this.lblAjuda = new System.Windows.Forms.Label();
+			this.lblRodar = new System.Windows.Forms.Label();
+			this.lblLista = new System.Windows.Forms.Label();
+			this.lblAdd = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Pokedex_Pokémon.Properties.Resources.Telemovel_Vertical_Pokedex;
+			this.pictureBox1.Location = new System.Drawing.Point(-6, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(260, 623);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Silver;
+			this.panel1.Controls.Add(this.lblInfo);
+			this.panel1.Controls.Add(this.lblAjuda);
+			this.panel1.Controls.Add(this.lblRodar);
+			this.panel1.Controls.Add(this.lblLista);
+			this.panel1.Controls.Add(this.lblAdd);
+			this.panel1.Location = new System.Drawing.Point(41, 226);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(175, 266);
+			this.panel1.TabIndex = 4;
+			this.panel1.Visible = false;
+			// 
+			// lblInfo
+			// 
+			this.lblInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.lblInfo.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblInfo.Location = new System.Drawing.Point(14, 191);
+			this.lblInfo.Name = "lblInfo";
+			this.lblInfo.Size = new System.Drawing.Size(150, 52);
+			this.lblInfo.TabIndex = 4;
+			this.lblInfo.Text = "Informações do Telemovel";
+			this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblAjuda
+			// 
+			this.lblAjuda.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.lblAjuda.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.lblAjuda.Location = new System.Drawing.Point(-37, 151);
+			this.lblAjuda.Name = "lblAjuda";
+			this.lblAjuda.Size = new System.Drawing.Size(268, 28);
+			this.lblAjuda.TabIndex = 3;
+			this.lblAjuda.Text = "Ajuda";
+			this.lblAjuda.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblRodar
+			// 
+			this.lblRodar.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.lblRodar.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.lblRodar.Location = new System.Drawing.Point(-37, 109);
+			this.lblRodar.Name = "lblRodar";
+			this.lblRodar.Size = new System.Drawing.Size(268, 28);
+			this.lblRodar.TabIndex = 2;
+			this.lblRodar.Text = "Rodar Ecra";
+			this.lblRodar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblLista
+			// 
+			this.lblLista.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.lblLista.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.lblLista.Location = new System.Drawing.Point(-37, 64);
+			this.lblLista.Name = "lblLista";
+			this.lblLista.Size = new System.Drawing.Size(268, 28);
+			this.lblLista.TabIndex = 1;
+			this.lblLista.Text = "Lista Pokemon";
+			this.lblLista.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblAdd
+			// 
+			this.lblAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.lblAdd.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.lblAdd.Location = new System.Drawing.Point(-37, 24);
+			this.lblAdd.Name = "lblAdd";
+			this.lblAdd.Size = new System.Drawing.Size(268, 28);
+			this.lblAdd.TabIndex = 0;
+			this.lblAdd.Text = "Add Pokemon";
+			this.lblAdd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(173, 181);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(32, 28);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 5;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Visible = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(47, 187);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(28, 24);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 5;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Visible = false;
+			// 
+			// FormAuxVertical
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(255, 637);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "FormAuxVertical";
+			this.Text = "FormAuxVertical";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label lblInfo;
+		private System.Windows.Forms.Label lblAjuda;
+		private System.Windows.Forms.Label lblRodar;
+		private System.Windows.Forms.Label lblLista;
+		private System.Windows.Forms.Label lblAdd;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+	}
+}
---
+++ Pokedex_Pokemon/Pokedex Pokémon/Pokedex Pokémon/frmMenssageBox.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Pokedex_Pokémon
+{
+	partial class frmMenssageBox
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.picIcon = new System.Windows.Forms.PictureBox();
+			this.lblText = new System.Windows.Forms.Label();
+			this.btn1 = new System.Windows.Forms.Button();
+			this.btn2 = new System.Windows.Forms.Button();
+			this.btn = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// picIcon
+			// 
+			this.picIcon.Location = new System.Drawing.Point(23, 30);
+			this.picIcon.Name = "picIcon";
+			this.picIcon.Size = new System.Drawing.Size(70, 51);
+			this.picIcon.TabIndex = 0;
+			this.picIcon.TabStop = false;
+			// 
+			// lblText
+			// 
+			this.lblText.Location = new System.Drawing.Point(112, 30);
+			this.lblText.Name = "lblText";
+			this.lblText.Size = new System.Drawing.Size(267, 51);
+			this.lblText.TabIndex = 1;
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(137, 11);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(75, 23);
+			this.btn1.TabIndex = 2;
+			this.btn1.Text = "button1";
+			this.btn1.UseVisualStyleBackColor = true;
+			// 
+			// btn2
+			// 
+			this.btn2.Location = new System.Drawing.Point(222, 11);
+			this.btn2.Name = "btn2";
+			this.btn2.Size = new System.Drawing.Size(75, 23);
+			this.btn2.TabIndex = 3;
+			this.btn2.Text = "button2";
+			this.btn2.UseVisualStyleBackColor = true;
+			// 
+			// btn
+			// 
+			this.btn.Location = new System.Drawing.Point(304, 11);
+			this.btn.Name = "btn";
+			this.btn.Size = new System.Drawing.Size(75, 23);
+			this.btn.TabIndex = 4;
+			this.btn.Text = "button3";
+			this.btn.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Silver;
+			this.panel1.Controls.Add(this.btn2);
+			this.panel1.Controls.Add(this.btn);
+			this.panel1.Controls.Add(this.btn1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 100);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(391, 46);
+			this.panel1.TabIndex = 5;
+			// 
+			// frmMenssageBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(391, 146);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.lblText);
+			this.Controls.Add(this.picIcon);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "frmMenssageBox";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox picIcon;
+		private System.Windows.Forms.Label lblText;
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.Button btn2;
+		private System.Windows.Forms.Button btn;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
