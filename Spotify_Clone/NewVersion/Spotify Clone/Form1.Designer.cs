1e55ab325e1d9f5378e9b034626a18a7709ab669
André Custódio <andrecustodiotablet@gmail.com>
2021-07-10 22:42:40 +0100

    Update old files of Spotify Clone and create new version

--- Spotify_Clone/NewVersion/Spotify Clone/Form1.Designer.cs
+++ Spotify_Clone/NewVersion/Spotify Clone/Form1.Designer.cs
@@ -29,28 +29,24 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			System.Windows.Forms.TrackBar trackBar1;
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.pnlTop = new System.Windows.Forms.Panel();
+			this.pE_MaxMin = new System.Windows.Forms.PictureBox();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.pE_minimizar = new System.Windows.Forms.PictureBox();
 			this.pE_Close = new System.Windows.Forms.PictureBox();
 			this.panel2 = new System.Windows.Forms.Panel();
+			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
 			this.pnlPrincipal = new System.Windows.Forms.Panel();
 			this.PainelControl1 = new System.Windows.Forms.Panel();
 			this.pnlPlayList = new System.Windows.Forms.Panel();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			this.pictureEdit3 = new System.Windows.Forms.PictureBox();
 			this.pnlControlo = new System.Windows.Forms.Panel();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.labelControl2 = new System.Windows.Forms.Label();
-			this.labelControl4 = new System.Windows.Forms.Label();
-			this.PBC = new System.Windows.Forms.ProgressBar();
-			this.pE_Repit = new System.Windows.Forms.PictureBox();
-			this.pE_Next = new System.Windows.Forms.PictureBox();
-			this.pE_previous = new System.Windows.Forms.PictureBox();
-			this.pE_PauseaPlay = new System.Windows.Forms.PictureBox();
-			this.pE_Random = new System.Windows.Forms.PictureBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.pnlConteudo = new System.Windows.Forms.Panel();
 			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
 			this.cmsMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -60,44 +56,26 @@
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.timer2 = new System.Windows.Forms.Timer(this.components);
 			this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
-			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-			trackBar1 = new System.Windows.Forms.TrackBar();
-			((System.ComponentModel.ISupportInitialize)(trackBar1)).BeginInit();
 			this.pnlTop.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pE_MaxMin)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pE_minimizar)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pE_Close)).BeginInit();
 			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
 			this.PainelControl1.SuspendLayout();
 			this.pnlPlayList.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureEdit3)).BeginInit();
 			this.pnlControlo.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Repit)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Next)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_previous)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_PauseaPlay)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Random)).BeginInit();
+			this.panel3.SuspendLayout();
 			this.pnlConteudo.SuspendLayout();
 			this.cmsMenuStrip.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// trackBar1
-			// 
-			trackBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-			resources.ApplyResources(trackBar1, "trackBar1");
-			trackBar1.CausesValidation = false;
-			trackBar1.Cursor = System.Windows.Forms.Cursors.Default;
-			trackBar1.Name = "trackBar1";
-			trackBar1.TabStop = false;
-			trackBar1.Value = 5;
-			trackBar1.Click += new System.EventHandler(this.trackBar1_Click);
-			trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
 			// 
 			// pnlTop
 			// 
 			this.pnlTop.BackColor = System.Drawing.Color.CadetBlue;
+			this.pnlTop.Controls.Add(this.pE_MaxMin);
 			this.pnlTop.Controls.Add(this.pictureBox1);
 			this.pnlTop.Controls.Add(this.pE_minimizar);
 			this.pnlTop.Controls.Add(this.pE_Close);
@@ -105,10 +83,19 @@
 			this.pnlTop.Name = "pnlTop";
 			this.pnlTop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseMove);
 			// 
+			// pE_MaxMin
+			// 
+			resources.ApplyResources(this.pE_MaxMin, "pE_MaxMin");
+			this.pE_MaxMin.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pE_MaxMin.Image = global::Spotify_Clone.Properties.Resources.expand__1_;
+			this.pE_MaxMin.Name = "pE_MaxMin";
+			this.pE_MaxMin.TabStop = false;
+			this.pE_MaxMin.Click += new System.EventHandler(this.pE_Click);
+			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.Image = global::Spotify_Clone.Properties.Resources.full_screen;
 			resources.ApplyResources(this.pictureBox1, "pictureBox1");
+			this.pictureBox1.Image = global::Spotify_Clone.Properties.Resources.full_screen;
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.TabStop = false;
 			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -120,7 +107,6 @@
 			this.pE_minimizar.Image = global::Spotify_Clone.Properties.Resources.minimize;
 			this.pE_minimizar.Name = "pE_minimizar";
 			this.pE_minimizar.TabStop = false;
-			this.pE_minimizar.Click += new System.EventHandler(this.pE_minimizar_Click);
 			// 
 			// pE_Close
 			// 
@@ -129,7 +115,7 @@
 			this.pE_Close.Image = global::Spotify_Clone.Properties.Resources.close_white;
 			this.pE_Close.Name = "pE_Close";
 			this.pE_Close.TabStop = false;
-			this.pE_Close.Click += new System.EventHandler(this.pE_Close_Click);
+			this.pE_Close.Click += new System.EventHandler(this.pE_Click);
 			this.pE_Close.MouseLeave += new System.EventHandler(this.pE_Close_MouseLeave);
 			this.pE_Close.MouseHover += new System.EventHandler(this.pE_Close_MouseHover);
 			// 
@@ -140,6 +126,14 @@
 			this.panel2.Controls.Add(this.pnlPrincipal);
 			resources.ApplyResources(this.panel2, "panel2");
 			this.panel2.Name = "panel2";
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			resources.ApplyResources(this.axWindowsMediaPlayer1, "axWindowsMediaPlayer1");
+			this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+			this.axWindowsMediaPlayer1.TabStop = false;
+			this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
 			// 
 			// pnlPrincipal
 			// 
@@ -180,89 +174,34 @@
 			// pnlControlo
 			// 
 			this.pnlControlo.BackColor = System.Drawing.Color.CadetBlue;
-			this.pnlControlo.Controls.Add(this.pictureBox2);
-			this.pnlControlo.Controls.Add(this.labelControl2);
-			this.pnlControlo.Controls.Add(trackBar1);
-			this.pnlControlo.Controls.Add(this.labelControl4);
-			this.pnlControlo.Controls.Add(this.PBC);
-			this.pnlControlo.Controls.Add(this.pE_Repit);
-			this.pnlControlo.Controls.Add(this.pE_Next);
-			this.pnlControlo.Controls.Add(this.pE_previous);
-			this.pnlControlo.Controls.Add(this.pE_PauseaPlay);
-			this.pnlControlo.Controls.Add(this.pE_Random);
+			this.pnlControlo.Controls.Add(this.panel3);
 			resources.ApplyResources(this.pnlControlo, "pnlControlo");
 			this.pnlControlo.Name = "pnlControlo";
 			// 
-			// pictureBox2
+			// panel3
 			// 
-			this.pictureBox2.Image = global::Spotify_Clone.Properties.Resources.info;
-			resources.ApplyResources(this.pictureBox2, "pictureBox2");
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.TabStop = false;
-			this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+			this.panel3.Controls.Add(this.panel5);
+			this.panel3.Controls.Add(this.panel4);
+			this.panel3.Controls.Add(this.panel1);
+			resources.ApplyResources(this.panel3, "panel3");
+			this.panel3.Name = "panel3";
 			// 
-			// labelControl2
+			// panel5
 			// 
-			resources.ApplyResources(this.labelControl2, "labelControl2");
-			this.labelControl2.Name = "labelControl2";
+			resources.ApplyResources(this.panel5, "panel5");
+			this.panel5.Name = "panel5";
 			// 
-			// labelControl4
+			// panel4
 			// 
-			resources.ApplyResources(this.labelControl4, "labelControl4");
-			this.labelControl4.Name = "labelControl4";
+			this.panel4.BackColor = System.Drawing.Color.Teal;
+			resources.ApplyResources(this.panel4, "panel4");
+			this.panel4.Name = "panel4";
 			// 
-			// PBC
+			// panel1
 			// 
-			resources.ApplyResources(this.PBC, "PBC");
-			this.PBC.Name = "PBC";
-			// 
-			// pE_Repit
-			// 
-			resources.ApplyResources(this.pE_Repit, "pE_Repit");
-			this.pE_Repit.BackColor = System.Drawing.Color.Transparent;
-			this.pE_Repit.Image = global::Spotify_Clone.Properties.Resources.refresh__3_;
-			this.pE_Repit.Name = "pE_Repit";
-			this.pE_Repit.TabStop = false;
-			this.pE_Repit.Tag = "0";
-			this.pE_Repit.Click += new System.EventHandler(this.pictureBox5_Click);
-			// 
-			// pE_Next
-			// 
-			resources.ApplyResources(this.pE_Next, "pE_Next");
-			this.pE_Next.BackColor = System.Drawing.Color.Transparent;
-			this.pE_Next.Image = global::Spotify_Clone.Properties.Resources.next;
-			this.pE_Next.Name = "pE_Next";
-			this.pE_Next.TabStop = false;
-			this.pE_Next.Click += new System.EventHandler(this.pictureBox4_Click);
-			// 
-			// pE_previous
-			// 
-			resources.ApplyResources(this.pE_previous, "pE_previous");
-			this.pE_previous.BackColor = System.Drawing.Color.Transparent;
-			this.pE_previous.Image = global::Spotify_Clone.Properties.Resources.previous;
-			this.pE_previous.Name = "pE_previous";
-			this.pE_previous.TabStop = false;
-			this.pE_previous.Click += new System.EventHandler(this.pE_previous_Click);
-			// 
-			// pE_PauseaPlay
-			// 
-			resources.ApplyResources(this.pE_PauseaPlay, "pE_PauseaPlay");
-			this.pE_PauseaPlay.BackColor = System.Drawing.Color.Transparent;
-			this.pE_PauseaPlay.Image = global::Spotify_Clone.Properties.Resources.play;
-			this.pE_PauseaPlay.Name = "pE_PauseaPlay";
-			this.pE_PauseaPlay.TabStop = false;
-			this.pE_PauseaPlay.Tag = "0";
-			this.pE_PauseaPlay.Click += new System.EventHandler(this.pE_PauseaPlay_Click);
-			// 
-			// pE_Random
-			// 
-			resources.ApplyResources(this.pE_Random, "pE_Random");
-			this.pE_Random.BackColor = System.Drawing.Color.Transparent;
-			this.pE_Random.Image = global::Spotify_Clone.Properties.Resources.change;
-			this.pE_Random.Name = "pE_Random";
-			this.pE_Random.TabStop = false;
-			this.pE_Random.Tag = "0";
-			this.pE_Random.Click += new System.EventHandler(this.pE_Random_Click);
+			this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+			resources.ApplyResources(this.panel1, "panel1");
+			this.panel1.Name = "panel1";
 			// 
 			// pnlConteudo
 			// 
@@ -302,21 +241,9 @@
 			// 
 			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
-			// timer1
-			// 
-			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-			// 
 			// timer2
 			// 
 			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
-			// 
-			// axWindowsMediaPlayer1
-			// 
-			resources.ApplyResources(this.axWindowsMediaPlayer1, "axWindowsMediaPlayer1");
-			this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-			this.axWindowsMediaPlayer1.TabStop = false;
-			this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
 			// 
 			// Form1
 			// 
@@ -328,25 +255,20 @@
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(trackBar1)).EndInit();
 			this.pnlTop.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pE_MaxMin)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pE_minimizar)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pE_Close)).EndInit();
 			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
 			this.PainelControl1.ResumeLayout(false);
 			this.pnlPlayList.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.pictureEdit3)).EndInit();
 			this.pnlControlo.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Repit)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Next)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_previous)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_PauseaPlay)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pE_Random)).EndInit();
+			this.panel3.ResumeLayout(false);
 			this.pnlConteudo.ResumeLayout(false);
 			this.cmsMenuStrip.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -365,14 +287,6 @@
 		private System.Windows.Forms.Panel pnlConteudo;
 		private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
 		private System.Windows.Forms.Panel pnlPrincipal;
-		private System.Windows.Forms.PictureBox pE_Repit;
-		private System.Windows.Forms.PictureBox pE_Next;
-		private System.Windows.Forms.PictureBox pE_previous;
-		private System.Windows.Forms.PictureBox pE_PauseaPlay;
-		private System.Windows.Forms.PictureBox pE_Random;
-		private System.Windows.Forms.ProgressBar PBC;
-		private System.Windows.Forms.Label labelControl4;
-		private System.Windows.Forms.Label labelControl2;
 		private System.ComponentModel.BackgroundWorker backgroundWorker1;
 		private System.Windows.Forms.ContextMenuStrip cmsMenuStrip;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
@@ -381,8 +295,12 @@
 		private System.Windows.Forms.ToolStripMenuItem TSMIEditPlayList;
 		private System.Windows.Forms.Timer timer2;
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.PictureBox pictureBox2;
 		private System.ComponentModel.BackgroundWorker backgroundWorker2;
+		private System.Windows.Forms.PictureBox pE_MaxMin;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
 	}
 }
 
